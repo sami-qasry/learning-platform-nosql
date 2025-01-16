f0f220f49df37b9a3b15027cf960aa08c0b08e53
EL-QASRY Sami <samielqasry2004@gmail.com>
2025-01-16 13:49:04 +0100

    Implémentation de la connexion Redis et une fonction pour la fermeture des connexions plus l'export des fonctions et les clients.

--- src/config/db.js
+++ src/config/db.js
@@ -26,9 +26,33 @@ async function connectMongo() {
 async function connectRedis() {
   // TODO: Implémenter la connexion Redis
   // Gérer les erreurs et les retries
+  try {
+    redisClient = redis.createClient({ url: config.redis.uri });
+    redisClient.on('error', (err) => console.error('Erreur Redis :', err));
+    await redisClient.connect();
+    console.log('Connexion à Redis réussie');
+  } catch (error) {
+    console.error('Erreur lors de la connexion à Redis :', error);
+    process.exit(1);
+  }
+}
+
+//implementation de la fermeture de la connexion
+function closeConnections() {
+  if (mongoClient) {
+    mongoClient.close().then(() => console.log('Connexion MongoDB fermée'));
+  }
+  if (redisClient) {
+    redisClient.quit().then(() => console.log('Connexion Redis fermée'));
+  }
 }
 
 // Export des fonctions et clients
 module.exports = {
   // TODO: Exporter les clients et fonctions utiles
+  connectMongo,
+  connectRedis,
+  closeConnections,
+  getMongoDb: () => db,
+  getRedisClient: () => redisClient
 };
