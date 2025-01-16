768a154fa093d4d123b09a6bcacacf7713478207
EL-QASRY Sami <samielqasry2004@gmail.com>
2025-01-16 12:57:14 +0100

    Implémentation de la connexion MongoDB

--- src/config/db.js
+++ src/config/db.js
@@ -12,6 +12,15 @@ let mongoClient, redisClient, db;
 async function connectMongo() {
   // TODO: Implémenter la connexion MongoDB
   // Gérer les erreurs et les retries
+  try {
+    mongoClient = new MongoClient(config.mongodb.uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoClient.connect();
+    db = mongoClient.db(config.mongodb.dbName);
+    console.log('Connexion à MongoDB réussie');
+  } catch (error) {
+    console.error('Erreur lors de la connexion à MongoDB :', error);
+    process.exit(1);
+  }
 }
 
 async function connectRedis() {
