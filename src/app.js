cf8b91bc6e2d8ae2f635268e10921d1d55781ba6
EL-QASRY Sami <samielqasry2004@gmail.com>
2025-01-16 18:17:28 +0100

    l'implementation de app.js

--- src/app.js
+++ src/app.js
@@ -13,9 +13,27 @@ const app = express();
 async function startServer() {
   try {
     // TODO: Initialiser les connexions aux bases de données
+    await db.connectMongo();
+    await db.connectRedis();
+
     // TODO: Configurer les middlewares Express
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
     // TODO: Monter les routes
-    // TODO: Démarrer le serveur
+    app.use('/api/courses', courseRoutes);
+    app.use('/api/students', studentRoutes);
+
+    app.get("/", (req, res, next) => {
+      res.json({ message: "test" })
+    })
+
+   
+    
+     // Démarrer le serveur
+     app.listen(config.port, () => {
+       console.log(`running on port ${config.port}`);
+     });
   } catch (error) {
     console.error('Failed to start server:', error);
     process.exit(1);
@@ -25,6 +43,8 @@ async function startServer() {
 // Gestion propre de l'arrêt
 process.on('SIGTERM', async () => {
   // TODO: Implémenter la fermeture propre des connexions
+  db.closeConnections();
+  process.exit(0);
 });
 
 startServer();
