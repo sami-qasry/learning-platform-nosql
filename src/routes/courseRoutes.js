0d9a77089140f3bb63ff10a6c9fbfd1ba23dc238
EL-QASRY Sami <samielqasry2004@gmail.com>
2025-01-16 17:44:16 +0100

    l'implementation des routes pour les cours

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -8,8 +8,11 @@ const router = express.Router();
 const courseController = require('../controllers/courseController');
 
 // Routes pour les cours
-router.post('/', courseController.createCourse);
-router.get('/:id', courseController.getCourse);
-router.get('/stats', courseController.getCourseStats);
+router.get('/', courseController.getAllCourses);
+router.post('/', courseController.createCourse); // POST to create a new course
+router.get('/stats', courseController.getCourseStats); // GET to retrieve course statistics
+router.get('/:id', courseController.getCourse); // GET to retrieve a course by ID
+router.put('/:id', courseController.updateCourse); // PUT to update a course by ID
+router.delete('/:id', courseController.deleteCourse);
 
 module.exports = router;
